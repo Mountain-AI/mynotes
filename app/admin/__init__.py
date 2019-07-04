173e2ab934af10d07301ed9f9db2f74c331d5b13
ZhangShanling <devezhang@gmail.com>
2019-07-04 19:53:41 +0800

    first commit

---
+++ app/admin/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# @Time    : 19-7-4 下午7:49
+# @Author  : zhangshanling
+
+
+if __name__ == '__main__':
+    pass
---
+++ app/tests/test.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+# @Time    : 19-7-4 下午7:48
+# @Author  : zhangshanling
