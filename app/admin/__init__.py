589db76aeb35f11f4b392368a860b958f922e284
ZhangShanling <devezhang@gmail.com>
2019-07-04 20:24:05 +0800

    review

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,6 +2,12 @@
 # @Time    : 19-7-4 下午7:49
 # @Author  : zhangshanling
 
+from flask import Blueprint
+
+admin = Blueprint('admin', __name__, template_folder="templates", static_url_path='', static_folder='static')
+
+# 在末尾导入相关模块，是为了避免循环导入依赖，因为在下面的模块中还要导入蓝本main
+from . import views, errors
 
 if __name__ == '__main__':
     pass
